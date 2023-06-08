c8ad9fb99c5ed3f7c94d4a735cdaffe2ac35a35c
maqiang877 <179964592@qq.com>
2023-06-08 11:09:15 +0800

    增加了公司信息first

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const OWNER = "maqiang877";
+export const REPO = "MindChat";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
